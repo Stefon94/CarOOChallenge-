93f160b744f9145ff662433601b3466b2aded7de
Steven Noto <stevennoto@Stevens-MacBook-Pro.local>
2024-04-19 10:11:34 -0400

    committed

---
+++ FruitSearch/FruitSearch.js
@@ -0,0 +1,32 @@
+
+
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+ function searchHandler() {
+const input = document.querySelector('#fruit').value.toLowerCase;
+const suggestions = document.querySelector('.suggestions');
+	suggestions.innerHTML = '';
+
+	if (input.textContent.length === 0) return;
+	const results = fruits.filter(result => result.startsWith(input));
+	results.forEach(result => {
+		const useSuggestion= document.createElement('div');
+		useSuggestion.textContent = result;
+	})
+}
+
+function showSuggestions(results, inputVal) {
+// array + element => suggestions 
+	// TODO
+}
+
+function useSuggestion(suggestion) {
+	
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion) => {
+	document.getElementById('input').value = result;
+	suggestions.innerHTML = '';
+};
